180c78b63f132a4325dac0b40406e7b575e07c3f
Lunkman728 <jstowers4187@stu.neisd.net>
2019-12-08 22:49:49 +0000

    Changed M2 to m1

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ y2=int(y2)
 # Variables cast into integers
 mY=int(y2-y1)
 mX=int(x2-x1)
-m2=int(mY/mX)
+m=int(mY/mX)
 if(x1<1):
-  b=int(y1-(x1*m2))
+  b=int(y1-(x1*m))
 elif(x1>1):
-  b=int((x1*m2)-y1)
+  b=int((x1*m)-y1)
 else:
-  b=int((m2*x1)-y1)
+  b=int((m*x1)-y1)
 # y=mx+b
 #B Inverter
 if(b>0):
@@ -37,7 +37,4 @@ if(b<0):
   print("y ="+" "+mY +"/"+mX+ "x " +  b )
 else:
   b=str(b)
-  print("y ="+" "+mY +"/"+mX+ "x" + " + " + b )  
-  #3=2/4x + B
-  #*2
-  #6=b
+  print("y ="+" "+mY +"/"+mX+ "x" + " + " + b )
