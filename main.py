22c0b355b13fd317118a40b2466b35fe1a6f20df
Lunkman728 <jstowers4187@stu.neisd.net>
2019-12-09 23:26:13 +0000

    Almost new program, but it has the b print

--- main.py
+++ main.py
@@ -4,23 +4,11 @@ x2=int(input("What is the second X value "))
 y1=int(input("What is the first Y value? "))
 y2=int(input("What is the second Y value? "))
 # Inputs are needed so the user can put in any numbers they want
-mY=int(y2-y1)
-mX=int(x2-x1)
-m=int(mY/mX)
-b=int(m*(-1)*x2+y2)
-print(b)
-#B Inverter
-if(x2>0):
-  b=int(b*1)
-  mY=str(mY)
-  mX=str(mX)
-elif(x2<0):
-  b=int(b*1)
-  mY=str(mY)
-  mX=str(mX)
-else:
-  mY=str(mY)
-  mX=str(mX)
+m=(y2-y1)/(x2-x1)
+
+
+
+
 if(b<0):
  b=str(b)
  print("y ="+" "+mY +"/"+mX+ "x " +  b )
