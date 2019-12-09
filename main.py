e3934f50494274c350aea43a4bcc8fe70e6015b2
Lunkman728 <jstowers4187@stu.neisd.net>
2019-12-09 20:35:26 +0000

    Comment Land :(

--- main.py
+++ main.py
@@ -1,50 +1,31 @@
 # To calculate slope, put in the x and y values.
-x1=input("What is the first X value? ")
-x2=input("What is the second X value ")
-y1=input("What is the first Y value? ")
-y2=input("What is the second Y value? ")
+x1=int(input("What is the first X value? "))
+x2=int(input("What is the second X value "))
+y1=int(input("What is the first Y value? "))
+y2=int(input("What is the second Y value? "))
 # Inputs are needed so the user can put in any numbers they want
-x1=int(x1)
-x2=int(x2)
-y1=int(y1)
-y2=int(y2)
-# Variables cast into integers
 mY=int(y2-y1)
 mX=int(x2-x1)
 m=int(mY/mX)
-if(x1<1):
-  b=int(y1-(x1*m2))
-  b=str(b)
-  print("b ="+ " "+ b)
-  b=int(b)
-elif(x1>1):
-  b=int(-1 * (m) * (x2) + y2)
-  b=str(b)
-  print("b ="+ " "+ b)
-  b=int(b)
-else:
-  b=int(-1 * (m) * (x2) + y2)
-  b=str(b)
-  print("b ="+ " "+ b)
-  b=int(b)
+b=int(m*(-1)*x1+y1)
+print(b)
 # y=mx+b
 #B Inverter
-if(b>0):
-  b=int(b*-1)
-  mY=str(mY)
-  mX=str(mX)
-elif(b<0):
-  b=int(b*-1)
-  mY=str(mY)
-  mX=str(mX)
-else:
-  b=int(0)
-mY=str(mY)
-mX=str(mX)
-if(b<0):
-  b=str(b)
-  print("y ="+" "+mY +"/"+mX+ "x " +  b )
-else:
-  b=str(b)
-  print("y ="+" "+mY +"/"+mX+ "x" + " + " + b )
-  
+#if(b>0):
+  #b=int(b*1)
+  #mY=str(mY)
+  #mX=str(mX)
+#elif(b<0):
+  #b=int(b*-1)
+  #mY=str(mY)
+  #mX=str(mX)
+#else:
+  #b=int(0)
+#mY=str(mY)
+#mX=str(mX)
+#if(b<0):
+# b=str(b)
+#print("y ="+" "+mY +"/"+mX+ "x " +  b )
+#else:
+ # b=str(b)
+  #print("y ="+" "+mY +"/"+mX+ "x" + " + " + b )
